00261fe49898275befa8fc1d098b0f0fee3a43e2
farmx009 <farmx009@morris.umn.edu>
2014-11-22 16:29:44 -0600

    Created restriction so that alert comes up when role of a user is being changed to a reviewer with same group as one of their advisees submissions, one of their own submissions, or when they are a copresenter for one of the submissions. -SF

--- client/app/roleChange/roleChange.controller.js
+++ client/app/roleChange/roleChange.controller.js
@@ -2,7 +2,7 @@
 'use strict';
 
 angular.module('umm3601ursamajorApp')
-    .controller('RoleChangeCtrl', function ($scope, $http, Auth, User, $location) {
+    .controller('RoleChangeCtrl', function ($scope, $http, Auth, User, $location, $filter) {
         if(Auth.isAdmin() || Auth.isChair()) {
         } else{
             $location.path('/');
@@ -18,6 +18,11 @@ angular.module('umm3601ursamajorApp')
 //    $http.get('/api/submissions').success(function(submissions) {
 //        $scope.submissions = submissions;
 //    });
+
+        $http.get('/api/submissions').success(function(submissions) {
+            $scope.submissions = submissions;
+            socket.syncUpdates('submission', $scope.submissions);
+        });
 
         $scope.roleOptions =
             [   'chair',
@@ -43,7 +48,7 @@ angular.module('umm3601ursamajorApp')
         };
 
 
-        $scope.delete = function(user) {
+        $scope.deleteUser = function(user) {
             if (Auth.getCurrentUser().email === user.email){
                 alert('Cannot delete yourself.');
             }
@@ -57,8 +62,22 @@ angular.module('umm3601ursamajorApp')
             }
         };
 
+        $scope.checkForConflict = function(user) {
+            if (
+                $filter('filter')($scope.submissions,
+                    function(submission) {
+                        if(submission == null) return false;
+                        return user.group === submission.group || user.email === submission.presenterInfo.email || user.email === submission.adviserInfo.email;
+                    }
+                ).length > 0
+                ) {
+                alert('Conflict with user and role.');
+            }
+        };
+
         $scope.updateInfo = function(user) {
             console.log(user);
+            $scope.checkForConflict(user);
             if (Auth.getCurrentUser().email === user.email){
                 alert('Cannot change user role for yourself.');
             }
@@ -73,7 +92,7 @@ angular.module('umm3601ursamajorApp')
 
         $scope.changeGroup = function(user) {
             console.log(user.group, user.role);
-            if(confirm('Are you sure you want to update this users group?')) {
+            if(confirm('Are you sure you want to update this usersJacob Opdahl  group?')) {
                 Auth.changeGroup(user.group, user);
             }
             console.log(user.group, user.role)
