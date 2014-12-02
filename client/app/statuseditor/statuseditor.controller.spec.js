e84afaed8af5323d569e4d508dffc023c79501bb
joseph <thele116@morris.umn.edu>
2014-12-01 20:41:38 -0600

    Fixed socket.io issue with testing (for now?)(Applied fix to all tests failing because of it).

--- client/app/statuseditor/statuseditor.controller.spec.js
+++ client/app/statuseditor/statuseditor.controller.spec.js
@@ -4,8 +4,10 @@ describe('Controller: StatuseditorCtrl', function () {
 
   // load the controller's module
   beforeEach(module('umm3601ursamajorApp'));
+  beforeEach(module('socketMock'));
 
-  var StatuseditorCtrl, scope;
+
+    var StatuseditorCtrl, scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
