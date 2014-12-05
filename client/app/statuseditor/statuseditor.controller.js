cb40bda32928d9981920f3366eb4f8217ffd426d
ricc0082 <ricc0082@umn.edu>
2014-12-05 13:54:55 -0500

    Option modal is a WIP, close to functionality.

--- client/app/statuseditor/statuseditor.controller.js
+++ client/app/statuseditor/statuseditor.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('umm3601ursamajorApp')
-  .controller('StatuseditorCtrl', function ($scope, $http, Auth, $location, User, Modal, socket) {
+    .controller('StatuseditorCtrl', function ($scope, $http, Auth, $location, User, Modal, socket) {
         if(Auth.isAdmin() || Auth.isChair()) {
         } else {
             $location.path('/');
@@ -48,8 +48,8 @@ angular.module('umm3601ursamajorApp')
 
         $scope.statusEditorColor = function(status){
             return {'border-left': '4px solid rgb(' + status.color.red   + ','
-                                                    + status.color.green + ','
-                                                    + status.color.blue  + ')'};
+                + status.color.green + ','
+                + status.color.blue  + ')'};
         };
 
         $scope.statusBoxColor = function(color){
@@ -67,19 +67,19 @@ angular.module('umm3601ursamajorApp')
         };
 
         $scope.deleteStatus = function(item){
-                $http.delete('/api/statuss/' + item._id).success(function () {
-                    $scope.statusArray.splice($scope.statusArray.indexOf(item), 1);
-                });
-                var threshold = item.priority;
-                for (var j = 0; j < $scope.statusArray.length; j++) {
-                    if ($scope.statusArray[j].priority != 15 && $scope.statusArray[j].priority != -15) {
-                        if ($scope.statusArray[j].priority > threshold) {
-                            $scope.statusArray[j].priority--;
-                            $http.patch('/api/statuss/' + $scope.statusArray[j]._id,
+            $http.delete('/api/statuss/' + item._id).success(function () {
+                $scope.statusArray.splice($scope.statusArray.indexOf(item), 1);
+            });
+            var threshold = item.priority;
+            for (var j = 0; j < $scope.statusArray.length; j++) {
+                if ($scope.statusArray[j].priority != 15 && $scope.statusArray[j].priority != -15) {
+                    if ($scope.statusArray[j].priority > threshold) {
+                        $scope.statusArray[j].priority--;
+                        $http.patch('/api/statuss/' + $scope.statusArray[j]._id,
                             {priority: $scope.statusArray[j].priority})
-                        }
                     }
                 }
+            }
         };
 
 
@@ -96,24 +96,27 @@ angular.module('umm3601ursamajorApp')
             return count;
         };
 
+        $scope.testConfirm = function(){
+            Modal.confirm.option(console.log("yes"),(console.log("no")))("Did the test work?");
+        };
+
 
         $scope.addStatusConfirm = function(){
             Modal.confirm.info($scope.addStatus)("Create a new status?");
         };
 
-
         $scope.addStatus = function() {
-                $http.post('/api/statuss/',
-                    {   strict: "Default Status",
-                        color: {red: 0, green: 0, blue: 0, alpha: 1},
-                        emailSubject: "",
-                        emailBody: "",
-                        priority: $scope.findEmptyPriority($scope.statusArray),
-                        required: false
-                    }).success(function () {
-                        console.log("Successfully added new status")
-                        $scope.getStatuses();
-                    });
+            $http.post('/api/statuss/',
+                {   strict: "Default Status",
+                    color: {red: 0, green: 0, blue: 0, alpha: 1},
+                    emailSubject: "",
+                    emailBody: "",
+                    priority: $scope.findEmptyPriority($scope.statusArray),
+                    required: false
+                }).success(function () {
+                    console.log("Successfully added new status")
+                    $scope.getStatuses();
+                });
         };
 
 
@@ -130,54 +133,53 @@ angular.module('umm3601ursamajorApp')
             var strict = "";
             var problem = false;
             var x = $scope.statusArray.indexOf(status);
-                for (var i = 0; i < $scope.statusArray.length; i++) {
-                    if ($scope.statusArray[i].priority == status.priority) {
-                        if ($scope.statusArray[i]._id != status._id) {
-                            problem = true;
-                        }
-
-                    }
-                    if ((status.priority <= 1 || status.priority >= 14)) {
+            for (var i = 0; i < $scope.statusArray.length; i++) {
+                if ($scope.statusArray[i].priority == status.priority) {
+                    if ($scope.statusArray[i]._id != status._id) {
                         problem = true;
-
-                    }
-                    if(status.required) {
-                        problem = false;
                     }
 
                 }
-                if (!problem) {
-                    $http.get('/api/statuss/' + $scope.statusArray[x]._id).success(function (oldStatus) {
-                        strict = oldStatus.strict;
+                if ((status.priority <= 1 || status.priority >= 14)) {
+                    problem = true;
 
-                        $http.patch('/api/statuss/' + $scope.statusArray[x]._id,
-                            {
-                                strict: $scope.statusArray[x].strict,
-                                color: $scope.statusArray[x].color,
-                                emailSubject: $scope.statusArray[x].emailSubject,
-                                emailBody: $scope.statusArray[x].emailBody,
-                                priority: $scope.statusArray[x].priority
-                            }
-                        ).success(function () {
-                                $location.path('/admin');
-                                for (var j = 0; j < $scope.submissions.length; j++) {
-                                    if ($scope.submissions[j].status.strict == strict) {
-                                        console.log("things were detected to be different");
-                                        $scope.submissions[j].status.strict = $scope.statusArray[x].strict;
-                                        $http.patch('/api/submissions/' + $scope.submissions[j]._id, {
-                                            status: {strict: $scope.statusArray[x].strict, text: $scope.submissions[j].status.text}
-                                        })
+                }
+                if(status.required) {
+                    problem = false;
+                }
 
-                                    }
+            }
+            if (!problem) {
+                $http.get('/api/statuss/' + $scope.statusArray[x]._id).success(function (oldStatus) {
+                    strict = oldStatus.strict;
+
+                    $http.patch('/api/statuss/' + $scope.statusArray[x]._id,
+                        {
+                            strict: $scope.statusArray[x].strict,
+                            color: $scope.statusArray[x].color,
+                            emailSubject: $scope.statusArray[x].emailSubject,
+                            emailBody: $scope.statusArray[x].emailBody,
+                            priority: $scope.statusArray[x].priority
+                        }
+                    ).success(function () {
+                            $location.path('/admin');
+                            for (var j = 0; j < $scope.submissions.length; j++) {
+                                if ($scope.submissions[j].status.strict == strict) {
+                                    console.log("things were detected to be different");
+                                    $scope.submissions[j].status.strict = $scope.statusArray[x].strict;
+                                    $http.patch('/api/submissions/' + $scope.submissions[j]._id, {
+                                        status: {strict: $scope.statusArray[x].strict, text: $scope.submissions[j].status.text}
+                                    })
+
                                 }
-                            })
-                    })
+                            }
+                        })
+                })
 
 
-                } else {
-                    Modal.confirm.warning()("There is a problem using this priority (priority is less than 2, greater than 14, or shares a priority with another status). Please, pick a new one.");
-                    //alert("There is a problem using this priority (priority is less than 2, greater than 14, or shares a priority with another status). Please, pick a new one.")
-                }
+            } else {
+                Modal.confirm.warning()("There is a problem using this priority (priority is less than 2, greater than 14, or shares a priority with another status). Please, pick a new one.");
+            }
         }
 
     });
