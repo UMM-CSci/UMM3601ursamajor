f3988acc77ee98a4ed2aa1374088d59c78f65a72
DjGusaas <gusaa004@morris.umn.edu>
2014-12-05 12:52:58 -0600

    Messed around with e2e tests. None of them work as of yet.

--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -1,16 +1,35 @@
 'use strict';
 
-//describe('Main View', function() {
+describe('Main View', function() {
 //  var page;
-//
+
+//  //Pre-generated
 //  beforeEach(function() {
 //    browser.get('/');
 //    page = require('./main.po');
 //  });
-//
+
 //  it('should include jumbotron with correct data', function() {
 //    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
 //    expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
 //    expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
 //  });
-//});
+
+//Added by Dalton
+//  browser.get('main.html');
+//
+//  it('should blah blah blah', function() {
+//     expect(browser.getLocationAbsUrl()).toMatch('/');
+//  });
+
+
+//  describe('login', function() {
+//       it('should redirect to login page', function() {
+//         expect(browser.getLocationLogin()).toMatch(';')
+//       });
+//
+//  });
+
+
+
+});
