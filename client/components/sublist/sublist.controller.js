45efce57a3efbd864d9163501f46b343cbbce2ec
DjGusaas <gusaa004@morris.umn.edu>
2014-12-09 15:51:36 -0600

    Added admin email to be sent and added some tests

--- client/components/sublist/sublist.controller.js
+++ client/components/sublist/sublist.controller.js
@@ -515,10 +515,10 @@ angular.module('umm3601ursamajorApp')
                 {cc: true}).success(function(){
                     $scope.selection.item.cc = true;
                     console.log("Successfully updated approval of submission with CC checked");
-                })
+                });
             sendGmailWithCC({
                 to: $scope.selection.item.presenterInfo.email + " " + $scope.selection.item.copresenterOneInfo.email + " " + $scope.selection.item.copresenterTwoInfo.email,
-                cc: $scope.selection.item.adviserInfo.email,
+                cc: $scope.selection.item.adviserInfo.email + "ursadmin@morris.umn.edu",
                 subject: "[" + $scope.selection.item.title + "] " + $scope.statusEdit.subject[$scope.statusEdit.options.indexOf($scope.selection.item.status.strict)],
                 message: $scope.selection.item.presenterInfo.first + ", your URS abstract has been approved by your adviser. Please await reviewer comments."
             });
@@ -545,10 +545,11 @@ angular.module('umm3601ursamajorApp')
                         //$scope.selection.item.status.text = status[i].text;
                         $http.patch('api/submissions/' + $scope.selection.item._id,
                             {approval: true,
-                                rejection: false,
-                                status: {strict: $scope.selection.item.status.strict, priority: $scope.statusEdit.priority[$scope.statusEdit.options.indexOf($scope.selection.item.status.strict)], text: "This URS submission has been approved by an adviser."}}
+                             rejection: false,
+                             status: {strict: $scope.selection.item.status.strict, priority: $scope.statusEdit.priority[$scope.statusEdit.options.indexOf($scope.selection.item.status.strict)], text: "This URS submission has been approved by an adviser."}}
                         ).success(function () {
                                 $scope.selection.item.approval = true;
+                                $scope.selection.item.rejection = false;
                                 console.log("Successfully updated approval of submission (approved)");
                             });
                     }
