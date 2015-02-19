9fd554a56d4afdc9522a90595e6e94913220ecb1
casal033 <casal033@morris.umn.edu>
2015-02-19 13:25:29 -0600

    Added more tests to status editor with Mark

--- client/app/statuseditor/statuseditor.controller.spec.js
+++ client/app/statuseditor/statuseditor.controller.spec.js
@@ -54,7 +54,14 @@ describe('Controller: StatuseditorCtrl', function () {
              emailBody: ", Your URS submission has either been rejected by your adviser or been withdrawn.",
              priority: 14,
              required: true
-         }
+         },{
+         strict: "EmptyStaus",
+         color: {red: 0, green: 0, blue: 0, alpha: 1},
+         emailSubject: "",
+         emailBody: "",
+         priority: null,
+         required: true
+       }
      ]
   });
 
@@ -71,4 +78,26 @@ describe('Controller: StatuseditorCtrl', function () {
     expect(scope.requiredStatus(scope.statusArray[1])).toEqual(false);
   })
 
+  describe('status editor', function(){
+    it('Status editor color' ,function(){
+      expect(scope.statusEditorColor(scope.statusArray[1])).toEqual({'border-left': '4px solid rgb(0,100,255)'});
+    });
+
+    it('Status box color' ,function(){
+      expect(scope.statusBoxColor(scope.statusArray[2].color)).toEqual({'background-color': 'rgb(255,220,10)'});
+    });
+
+    it('Color click' ,function(){
+      expect(scope.colorClick(scope.statusArray[1], {red: 194, green: 194, blue: 194, alpha: 1})).toEqual();
+    });
+
+    it('selected color' ,function(){
+      expect(scope.selectedColor(scope.statusArray[1], {red: 194, green: 194, blue: 194, alpha: 1})).toEqual(false);
+      expect(scope.selectedColor(scope.statusArray[1], {red: 0, green: 100, blue: 255, alpha: 1})).toEqual(true);
+    });
+
+    it('Find empty priority' ,function(){
+      expect(scope.findEmptyPriority(scope.statusArray)).toEqual(4);
+    });
+  });
 })
