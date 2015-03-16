0e049ad8fa6bc91e5651594ff469d190b81d383a
Jacoby04 <opdah023@morris.umn.edu>
2015-03-16 17:29:58 -0500

    Started making the change review group route. Right now, just modeling it after the edit users page that the admin has access to.

--- client/app/groupChange/groupChange.controller.js
+++ client/app/groupChange/groupChange.controller.js
@@ -1,6 +1,24 @@
 'use strict';
 
 angular.module('umm3601ursamajorApp')
-  .controller('GroupChangeCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('GroupChangeCtrl', function ($scope, Auth, $location, $http, socket) {
+
+    $scope.submissions = [];
+
+    if(!Auth.isAdmin() && !Auth.isChair()) {
+      $location.path('/');
+    }
+
+    $http.get('/api/submissions').success(function(submissions) {
+      $scope.submissions = submissions;
+      socket.syncUpdates('submission', $scope.submissions);
+    });
+
+    $scope.reviewGroupOptions =
+      [ 0,
+        1,
+        2,
+        3,
+        4
+      ];
   });
