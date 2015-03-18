453bbc69ab3b80f115d91db00b5dd127d3ecd5e9
Jacoby04 <opdah023@morris.umn.edu>
2015-03-18 18:33:46 -0400

    All the code related to changing the review group in sbulist has now been removed. REFER TO THIS COMMIT OR BEFORE IF THERE SEEMS TO BE BREAKS WITH REGARDS TO  THIS OLD CODE!!!

--- client/components/sublist/sublist.controller.js
+++ client/components/sublist/sublist.controller.js
@@ -742,43 +742,6 @@ angular.module('umm3601ursamajorApp')
                 })
         };
 
-
-        $scope.getReviewGroupMembers = function(group) {
-            return $filter('filter')($scope.users,
-                function(user) {
-                    return user.group == group;
-                })
-        };
-
-        // Todo: Update to include coadvisers...
-        $scope.checkForConflict = function(submission) {
-            console.log("checking for conflicts for: " + submission.title + " and review group " + $scope.selection.reviewGroup);
-            if (
-                $filter('filter')($scope.getReviewGroupMembers($scope.selection.reviewGroup),
-                    function(user) {
-                        return user.email === submission.copresenterOneInfo.email || user.email === submission.presenterInfo.email || user.email === submission.adviserInfo.email || user.email === submission.copresenterTwoInfo.email;
-                    }
-                ).length > 0
-                ) {
-                console.log("Conflict with submission and review group.");
-                Modal.confirm.warning()('Conflict with submission and review group.');
-            }
-        };
-
-        $scope.setReviewGroupConfirm = function(submission) {
-            Modal.confirm.info($scope.setReviewGroup)('Are you sure you want to change this submissions review group?', submission);
-        };
-
-        $scope.setReviewGroup = function(submission) {
-            $scope.checkForConflict(submission);
-                $http.patch('api/submissions/' + submission._id,
-                    {group: $scope.selection.reviewGroup}
-                ).success(function(){
-                        console.log("Successfully updated status of submission");
-                        submission.group = $scope.selection.reviewGroup;
-                    });
-        };
-
         $scope.resetTemps = function() {
             if($scope.selection.item != null){
                 $scope.statusEdit.temp.strict = $scope.selection.item.status.strict;
