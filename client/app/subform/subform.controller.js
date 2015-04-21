3687d4a037b88c93b346b0ff5abdac8cab8d1bb4
casal033 <casal033@morris.umn.edu>
2015-04-21 17:12:11 -0500

    Finished modifying wording on adviser notification email upon initial submission.

--- client/app/subform/subform.controller.js
+++ client/app/subform/subform.controller.js
@@ -220,8 +220,8 @@ angular.module('umm3601ursamajorApp')
                         ' has submitted a URS submission that requires your approval. ' +
                         'By approving the submission, you are authorizing the student(s) to submit this abstract for consideration by the Undergraduate Research Symposium. ' +
                         'This is not approving the final abstract; the student(s) will have the opportunity to further revise, improve, and finalize their submission. '+
-                        'Please go to https://ursa-major-project.herokuapp.com/ and login with your X500 UMN email. Once there, navigate to the "Submissions List" tab to approve (or reject) ' +
-                        'the submission.'
+                        'Please go to https://ursa-major-project.herokuapp.com/ and login with your X500 UMN email. Once there, navigate to the "Submissions List" tab and choose your advisees ' +
+                        'submission to approve (or reject) it. You will have the option to be included in future emails pertaining to the submission, please read carefully.'
                 });
             }
         };
