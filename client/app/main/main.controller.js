2c576dae9a8e3e37f10cc28c79bbe855977e3c5b
twarling <warli010@morris.umn.edu>
2017-08-11 15:52:20 -0500

    Added a pop up modal if the user has not picked a t-shirt size

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -12,4 +12,42 @@ angular.module('umm3601ursamajorApp')
     $scope.hasUMNEmail = function(){
       return Auth.getCurrentUser().email.indexOf("umn.edu") != -1;
     };
+
+    // An array of possible tShirtSize's.
+    $scope.tShirtSizeOptions =
+      [ "Small",
+        "Medium",
+        "Large",
+        "XLarge",
+        "XXLarge",
+        "XXXLarge"
+      ];
+
+    // Changes the tShirtSize designated to a user.
+    $scope.updateTShirtSize = function(){
+      Auth.updateTShirtSize($scope.user.tShirtSize);
+      Auth.getCurrentUser().tShirtSize = $scope.user.tShirtSize;
+    };
+
+    $scope.pickTShirtSize = function(){
+      if(Auth.getCurrentUser().tShirtSize == ""){
+        return true;
+      }
+      else{
+        return false;
+      }
+
+    }
+
+    // Get the modal
+    var modal = document.getElementById('tShirtModal');
+
+    // Opens modal if user has not picked a t-shirt size yet
+    $scope.openModal = function() {
+      modal.style.display = "block";
+
+    }
+
+
+
   });
